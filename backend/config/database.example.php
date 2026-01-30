ec0902ee701e2859983dccfcfd17391388d27012
Ahmad Munif <ahmad.munif02@gmail.com>
2026-01-30 21:40:40 +0700

    chore: Secure database credentials, remove from git

--- backend/config/database.example.php
+++ backend/config/database.example.php
@@ -1,12 +1,12 @@
 <?php
-// backend/config/database.php
+// backend/config/database.example.php
 
 class Database
 {
     private $host = "localhost";
-    private $db_name = "ouycwnsb_dev";
-    private $username = "ouycwnsb_admin";
-    private $password = "t_wn8LUzGHv88RA";
+    private $db_name = "your_db_name";
+    private $username = "your_db_user";
+    private $password = "your_db_password";
     public $conn;
 
     public function getConnection()
@@ -14,7 +14,6 @@ class Database
         $this->conn = null;
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch (PDOException $exception) {
