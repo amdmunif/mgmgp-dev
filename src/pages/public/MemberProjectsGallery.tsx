cdd571ad33c36ff48b47484eeab14592f09cd87d
Ahmad Munif <ahmad.munif02@gmail.com>
2026-08-26 17:19:51 +0700

    style: update vertical padding in MemberProjectsGallery to py-8

--- src/pages/public/MemberProjectsGallery.tsx
+++ src/pages/public/MemberProjectsGallery.tsx
@@ -34,7 +34,7 @@ export function MemberProjectsGallery() {
     }
 
     return (
-        <div className="min-h-screen bg-gray-50 pt-24 pb-20">
+        <div className="min-h-screen bg-gray-50 py-8">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 
                 <div className="text-center max-w-3xl mx-auto mb-16">
