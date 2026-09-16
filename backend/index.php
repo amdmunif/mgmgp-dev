a510bdaefe671d133292c191b7df122717bcd15c
Ahmad Munif <ahmad.munif02@gmail.com>
2026-09-16 11:51:05 +0700

    feat: parse subAction from URI for board-meetings route

--- backend/index.php
+++ backend/index.php
@@ -630,6 +630,7 @@ if ($resource === 'news') {
 
 } elseif ($resource === 'board-meetings') {
     $controller = new BoardMeetingController();
+    $subAction = isset($uri_parts[2]) ? $uri_parts[2] : null;
     
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         if ($action) {
